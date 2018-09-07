e95e69083046e0320f00373e0731d974aafc882a
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-07 23:30:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,4 +46,10 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+  
 end
