e7c43291f40df40eb3bd61c263c7ea294cd85262
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-07 23:25:03 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,4 +30,8 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
 end
